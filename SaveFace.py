320e469faa9bfa3e649076a23a22baffcfe06f95
Rob <120913030+RoT1337@users.noreply.github.com>
2025-04-09 23:28:47 +0800

    Initial commit

---
+++ SaveFace.py
@@ -0,0 +1,60 @@
+import cv2
+import tkinter as tk
+from tkinter import simpledialog
+import tkinter.messagebox as messagebox
+import os
+
+def get_user_name():
+    root = tk.Tk()
+    root.withdraw()
+    name = simpledialog.askstring("Input", "Enter your name:")
+    return name
+
+def capture_picture():
+    index = 0
+
+    name = get_user_name()
+    if (not name):
+        print("No name entered. Exiting...")
+        return
+    
+    messagebox.showinfo("Welcome", f"Hello {name}, position yourself in front of the camera and press 's' to take a picture.")
+
+    save_dir = "FaceRecognition/faces"
+    if (not os.path.exists(save_dir)):
+        os.makedirs(save_dir)
+
+    webcam = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = webcam.read()
+        if (not ret):
+            print("Failed to access the webcam or webcam was closed.")
+            break
+
+        while True:
+            ret, frame = webcam.read()
+            if (not ret):
+                print("Failed to access the webcam.")
+                break
+
+            cv2.imshow("Webcam - Press 's' to take a picture, 'q' to quit", frame)
+
+            key = cv2.waitKey(1) & 0xFF
+
+            if (key == ord('s')):
+                file_name = os.path.join(save_dir, f"{name}_{index}.jpg")
+                cv2.imwrite(file_name, frame)
+                messagebox.showinfo("Successful", f"Picture saved as {file_name}")
+                index += 1
+            elif (key == ord('q')):
+                messagebox.showinfo("Error", "Closing the application, press OK")
+                webcam.release()
+                cv2.destroyAllWindows()
+                break
+
+    webcam.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    capture_picture()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+ # for (x , y, w, h) in noses:
+    #     cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 1)
